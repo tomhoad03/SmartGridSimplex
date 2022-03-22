037a6c2b0b579160097ac416d4df0b1aca773a78
Tom Hoad <tom.hoad03@gmail.com>
2022-03-22 16:41:34 +0000

    K means classification

--- src/ManipulationDetection.java
+++ src/ManipulationDetection.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 
 public class ManipulationDetection {
     public static void main(String[] args) {
-        ArrayList<PricingCurve> trainingPricingCurves = new ArrayList<>();
-        ArrayList<PricingCurve> testingPricingCurves = new ArrayList<>();
+        ArrayList<PricingCurve> trainingPricingCurves = new ArrayList<>(10000);
+        ArrayList<PricingCurve> testingPricingCurves = new ArrayList<>(100);
 
+        // Read in the training data
         try {
-            // Read in the training data
             File trainingFile = new File("TrainingData.txt");
             Scanner trainingReader = new Scanner(trainingFile);
 
+            // For each line, create a pricing curve object from the training data
             while (trainingReader.hasNextLine()) {
                 ArrayList<String> stringValues = new ArrayList<>(Arrays.asList(trainingReader.nextLine().split(",")));
 
@@ -25,11 +26,16 @@ public class ManipulationDetection {
                 trainingPricingCurves.add(new PricingCurve(doubleValues, normal));
             }
             trainingReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-            // Read in the testing data
+        // Read in the testing data
+        try {
             File testingFile = new File("TestingData.txt");
             Scanner testingReader = new Scanner(testingFile);
 
+            // For each line, create a pricing curve object from the testing data
             while (testingReader.hasNextLine()) {
                 ArrayList<String> stringValues = new ArrayList<>(Arrays.asList(testingReader.nextLine().split(",")));
 
@@ -38,9 +44,63 @@ public class ManipulationDetection {
 
                 testingPricingCurves.add(new PricingCurve(doubleValues));
             }
-            trainingReader.close();
+            testingReader.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        // Calculate the centroid values of the normal and abnormal values
+        ArrayList<Double> normalCumulativeValues = new ArrayList<>(24), abnormalCumulativeValues = new ArrayList<>(24);
+        ArrayList<Double> normalCentroidValues = new ArrayList<>(24), abnormalCentroidValues = new ArrayList<>(24);
+
+        for (PricingCurve trainingPricingCurve : trainingPricingCurves) {
+            if (trainingPricingCurve.isNormal()) {
+                populateCumulativeValues(normalCumulativeValues, trainingPricingCurve);
+            } else {
+                populateCumulativeValues(abnormalCumulativeValues, trainingPricingCurve);
+            }
+        }
+
+        for (int i = 0; i < 24; i++) {
+            normalCentroidValues.add(normalCumulativeValues.get(i) / 5000);
+            abnormalCentroidValues.add(abnormalCumulativeValues.get(i) / 5000);
+        }
+
+        // Classify the testing data
+        for (PricingCurve testingPricingCurve : testingPricingCurves) {
+            double normalDistanceSquared = 0, abnormalDistanceSquared = 0;
+
+            for (int i = 0; i < 24; i++) {
+                normalDistanceSquared += Math.pow((testingPricingCurve.getPricingValues().get(i) - normalCentroidValues.get(i)), 2);
+                abnormalDistanceSquared += Math.pow((testingPricingCurve.getPricingValues().get(i) - abnormalCentroidValues.get(i)), 2);
+            }
+
+            if (normalDistanceSquared < abnormalDistanceSquared) {
+                testingPricingCurve.setNormal();
+            }
+        }
+
+        // Calculate stats from testing
+        int normalCount = 0, abnormalCount = 0;
+        for (int i = 0; i < 100; i++) {
+            if (testingPricingCurves.get(i).isNormal()) {
+                normalCount++;
+            } else {
+                abnormalCount++;
+            }
+        }
+        System.out.println("Normal: " + normalCount + ", Abnormal: " + abnormalCount);
+    }
+
+    private static void populateCumulativeValues(ArrayList<Double> cumulativeValues, PricingCurve trainingPricingCurve) {
+        if (cumulativeValues.size() == 0) {
+            for (int i = 0; i < 24; i++) {
+                cumulativeValues.add(trainingPricingCurve.getPricingValues().get(i));
+            }
+        } else {
+            for (int i = 0; i < 24; i++) {
+                cumulativeValues.set(i, cumulativeValues.get(i) + trainingPricingCurve.getPricingValues().get(i));
+            }
         }
     }
 }
--- src/PricingCurve.java
+++ src/PricingCurve.java
@@ -2,35 +2,26 @@ import java.util.ArrayList;
 
 public class PricingCurve {
     private final ArrayList<Double> pricingValues;
-    private final boolean isTesting;
     private boolean isNormal;
 
     public PricingCurve(ArrayList<Double> pricingValues, int normal) {
         this.pricingValues = pricingValues;
         this.isNormal = normal == 0;
-        this.isTesting = false;
     }
 
     public PricingCurve(ArrayList<Double> pricingValues) {
         this.pricingValues = pricingValues;
-        this.isTesting = true;
     }
 
     public ArrayList<Double> getPricingValues() {
         return pricingValues;
     }
 
-    public boolean isTesting() {
-        return isTesting;
-    }
-
     public boolean isNormal() {
         return isNormal;
     }
 
-    public void setNormal(boolean normal) {
-        if (isTesting) {
-            isNormal = normal;
-        }
+    public void setNormal() {
+        isNormal = true;
     }
 }
